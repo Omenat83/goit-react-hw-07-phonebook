57a7589e98bcb23e6fb9c3d4442fa6e37168eb75
Maryna K <110280285+Omenat83@users.noreply.github.com>
2023-09-21 17:02:41 +0300

    Update fetch.js

--- src/services/fetch.js
+++ src/services/fetch.js
@@ -1,9 +1,9 @@
 import axios from 'axios';
 
-axios.defaults.baseURL = 'https://645a3f0565bd868e931543d7.mockapi.io/contacts';
+axios.defaults.baseURL = 'https://64661aec9c09d77a62fd188a.mockapi.io/contacts';
 
 export async function getContacts() {
-  const {data} =await axios.get('/');
+  const { data } = await axios.get('/');
   return data;
 }
 
